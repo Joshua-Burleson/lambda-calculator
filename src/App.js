8cae3a025ef9439bd2dc47bdbc2af6daf9faf2a9
Joshua-Burleson <joshua.burleson.developer@gmail.com>
2019-09-08 13:33:27 -0400

    Scope error resolved

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Operators from './components/ButtonComponents/OperatorButtons/Operators.j
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
-import Logo from "./components/DisplayComponents/Logo";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -5,11 +5,11 @@ import SpecialButton from './SpecialButton.js';
 //import any components needed
 
 //Import your array data to from the provided data file
-const [specialState, setSpecialState] = useState(props.specials);
-const specialsButtonsArray = props.specials.map(special => <SpecialButton symbol = {special} />);
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
+  const [specialState, setSpecialState] = useState(props.specials);
+  const specialsButtonsArray = props.specials.map(special => <SpecialButton symbol = {special} />);
 
   return (
     <div>
