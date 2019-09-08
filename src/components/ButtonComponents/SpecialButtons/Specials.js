7f4b3a80ad5dfca79b430a0eef74fc735a43891d
Joshua-Burleson <joshua.burleson.developer@gmail.com>
2019-09-08 13:38:30 -0400

    Scope error resolved

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -6,7 +6,7 @@ import SpecialButton from './SpecialButton.js';
 
 //Import your array data to from the provided data file
 
-const Specials = () => {
+const Specials = (props) => {
   // STEP 2 - add the imported data to state
   const [specialState, setSpecialState] = useState(props.specials);
   const specialsButtonsArray = props.specials.map(special => <SpecialButton symbol = {special} />);
