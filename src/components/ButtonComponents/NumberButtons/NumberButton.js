0a20e6cdb917678e0ec536f9fe1da867c597b5be
Joshua-Burleson <joshua.burleson.developer@gmail.com>
2019-09-10 00:56:20 -0400

    Updating CSS 2/3

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const NumberButton = (props) => {
   return (
-    <button onClick = {() => {props.onClick(props.number)}}>
+    <button onClick = {() => {props.onClick(props.number)}} className = {String(props.number) === "0" ? "zero" : "num"}>
       {props.number}
     </button>
   );
